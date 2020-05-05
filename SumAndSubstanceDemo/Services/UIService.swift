e059d49285d0b69350576cf1e4d21ed526294335
Andrey Krasivichev <21ghost@mail.ru>
2020-05-05 19:43:24 +0300

    test reversed

--- SumAndSubstanceDemo/Services/UIService.swift
+++ SumAndSubstanceDemo/Services/UIService.swift
@@ -30,8 +30,7 @@ class UIService: NSObject, ServicesProviderUsage {
     
     func showIdensicScreen(token: String, applicantId: String) {
         
-        let testToken = "7j2Gfy88e1f1LurtWS92L1fd152BG39VSjWq6zYDvQyVAKJsWXK3aA=="
-        let sdk = SNSMobileSDK(baseUrl: self.apiService.baseUrl, applicantId: applicantId, accessToken: testToken, locale: nil, supportEmail: nil)
+        let sdk = SNSMobileSDK(baseUrl: self.apiService.baseUrl, applicantId: applicantId, accessToken: token, locale: nil, supportEmail: nil)
         self.idensicSDK = sdk
         guard sdk.isReady else {
             self.showErrorAlertMessage(sdk.verboseStatus)
