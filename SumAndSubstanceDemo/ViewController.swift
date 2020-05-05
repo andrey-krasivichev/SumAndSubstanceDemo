08aed944c4084fd729e01cc79152a1daff6f24cd
Andrey Krasivichev <21ghost@mail.ru>
2020-05-05 11:13:44 +0300

    Initial Commit

---
+++ SumAndSubstanceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SumAndSubstanceDemo
+//
+//  Created by Andrey Krasivichev on 05.05.2020.
+//  Copyright © 2020 Andrey Krasivichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
